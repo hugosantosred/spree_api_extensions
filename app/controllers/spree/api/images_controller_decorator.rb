698093c3b3f07be64ca0380dfc36d2ecf03b2361
Hugo Santos <hugo.santos@factorlibre.com>
2013-03-05 12:29:46 +0100

    Fix Spree::Image object

--- app/controllers/spree/api/images_controller_decorator.rb
+++ app/controllers/spree/api/images_controller_decorator.rb
@@ -1,6 +1,6 @@
 Spree::Api::ImagesController.class_eval do
   def index
-    images = Image.find(:all, :conditions => ['viewable_type = ?', 'Spree::Variant'])
+    images = Spree::Image.find(:all, :conditions => ['viewable_type = ?', 'Spree::Variant'])
     @images = Kaminari.paginate_array(images).page(params[:page]).per(params[:per_page])
     respond_with(@images)
   end
